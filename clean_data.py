eb9819c469b39c71d05972839b2353cd5de34bf4
Pedr0ML <pemarinl@unal.edu.co>
2024-03-08 18:55:40 -0500

    intento01

--- clean_data.py
+++ clean_data.py
@@ -6,6 +6,10 @@ import pandas as pd
 
 def load_data(input_file):
     """Lea el archivo usando pandas y devuelva un DataFrame"""
+    df = pd.read_csv(input_file)
+    return df
+
+
 
 
 def create_fingerprint(df):
@@ -20,12 +24,34 @@ def create_fingerprint(df):
     # 7. Transforme cada palabra con un stemmer de Porter
     # 8. Ordene la lista de tokens y remueve duplicados
     # 9. Convierta la lista de tokens a una cadena de texto separada por espacios
+    df = df.copy()
+    df["key"] = df["text"]
+    df["key"] = df["key"].str.strip()
+    df["key"] = df["key"].str.lower()
+    df["key"] = df["key"].str.replace("-", "")
+    df["key"] = df["key"].str.translate(
+        str.maketrans("", "", "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~")
+    )
+    df["key"] = df["key"].str.split()
+    stemmer = nltk.PorterStemmer()
+    df["key"] = df["key"].apply(lambda x: [stemmer.stem(word) for word in x])
+    df["key"] = df["key"].apply(lambda x: sorted(set(x)))
+    df["key"] = df["key"].str.join(" ")
+    return df    
+
 
 
 def generate_cleaned_column(df):
     """Crea la columna 'cleaned' en el DataFrame"""
 
+
     df = df.copy()
+    df = df.sort_values(by=["key", "text"], ascending=[True, True])
+    keys = df.drop_duplicates(subset="key", keep="first")
+    key_dict = dict(zip(keys["key"], keys["text"]))
+    df["cleaned"] = df["key"].map(key_dict)
+    
+    return df
 
     # 1. Ordene el dataframe por 'fingerprint' y 'text'
     # 2. Seleccione la primera fila de cada grupo de 'fingerprint'
@@ -33,11 +59,15 @@ def generate_cleaned_column(df):
     # 4. Cree la columna 'cleaned' usando el diccionario
 
 
+
 def save_data(df, output_file):
     """Guarda el DataFrame en un archivo"""
     # Solo contiene una columna llamada 'texto' al igual
     # que en el archivo original pero con los datos limpios
-
+    df = df.copy()
+    df = df[["cleaned"]]
+    df = df.rename(columns={"cleaned": "text"})
+    df.to_csv(output_file, index=False)
 
 def main(input_file, output_file):
     """Ejecuta la limpieza de datos"""
